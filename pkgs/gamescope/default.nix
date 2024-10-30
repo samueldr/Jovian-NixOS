239ea5243499099971ba926b162a5e6cd71dd2e5
Samuel Dionne-Riel <samuel@dionne-riel.com>
2024-10-30 02:15:39 -0400

    gamescope: Add QAM synthesis patch
    This patch aims to make the QAM accessible from in-game via a well-known
    keyboard binding. The binding chosen is F22, since it shouldn't clash
    with any usual keyboard keys.

--- pkgs/gamescope/default.nix
+++ pkgs/gamescope/default.nix
@@ -1,9 +1,11 @@
 { gamescope'
+, fetchpatch
 , fetchFromGitHub
 }:
 
 # NOTE: vendoring gamescope for the time being since we want to match the
 #       version shipped by the vendor, ensuring feature level is equivalent.
+#       We're also patching-in features.
 
 gamescope'.overrideAttrs(old: rec {
   version = "3.15.14";
@@ -15,4 +17,12 @@ gamescope'.overrideAttrs(old: rec {
     fetchSubmodules = true;
     hash = "sha256-LVwwkISokjSXEYd/SFRtCDDY6P2sr6pQp8Xb8BsrXAw=";
   };
+
+  patches = old.patches ++ [
+    # wlserver: Sythesize QAM combo on F22
+    (fetchpatch {
+      url = "https://github.com/ValveSoftware/gamescope/commit/3df5a1bc977d36b68e15371b25dadc0380ffc9e0.patch";
+      hash = "sha256-BbwAxJYPulOVB7m17R9yskwOePprSwoqKJ1V7dEASJI=";
+    })
+  ];
 })
